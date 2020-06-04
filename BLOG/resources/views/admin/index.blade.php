280a13d66fb736f0b4962d27feb9228b2a5808f8
adamsandlerisfunny <lnicolaidou.katerina@gmail.com>
2020-06-04 22:07:14 +0100

    Minor post tweaks

--- BLOG/resources/views/admin/index.blade.php
+++ BLOG/resources/views/admin/index.blade.php
@@ -17,7 +17,8 @@
     @foreach($posts as $post)
         <div class="row">
             <div class="col-md-12">
-                <p><strong><a href="{{ route('blog.post', ['id' => $post ->id]) }}">{{ $post->title }}</a></strong>
+                <p><strong>{{ $post->title }}</strong>
+                <a href="{{ route('blog.post', ['id' => $post ->id]) }}"> View </a>
                 @if (auth()->user()->id === $post->user_id)
                     <a href="{{ route('admin.edit', ['id' => $post ->id]) }}">Edit</a>
                     <a href="{{ route('admin.delete', ['id' => $post ->id]) }}">Delete</a>       
